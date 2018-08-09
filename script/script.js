a65fb12f06ab52a2f29ad9d85c1fcf7aefb7f5c4
Paweł <37835885+pawelmusial1995@users.noreply.github.com>
2018-08-09 18:38:25 +0200

    another-day-with-code

---
+++ script/script.js
@@ -0,0 +1,7 @@
+//BUTTON SCROLL DOWN FUNCTION 
+$(function() {
+    $('.scroll-down').click (function() {
+      $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
+      return false;
+    });
+  });
